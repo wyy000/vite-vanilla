1eb608330c9389b307f8c8c96263377c7a02a723
wyy000 <sxd_wangyangyang@csmail.citicbank.com>
2021-02-22 09:49:44 +0800

    refactor: require引入模块方式修改

--- require/js/script/chart.js
+++ require/js/script/chart.js
@@ -1,86 +1,84 @@
 require(['common'], function () {
-  require(['jquery'],function($) {
-    require(['echarts'], function (echarts) {
-      // 基于准备好的dom，初始化echarts实例
-      const chart = $('#chart_main')
-      const $child = $('<div id="child"></div>')
-      const $parent = $('<div id="parent"></div>')
+  require(['jquery', 'echarts'],function($, echarts) {
+    // 基于准备好的dom，初始化echarts实例
+    const chart = $('#chart_main')
+    const $child = $('<div id="child"></div>')
+    const $parent = $('<div id="parent"></div>')
 
-      chart.append($child, $parent)
+    chart.append($child, $parent)
 
-      $child
-        .css('width', '600px')
-        .css('height', '400px')
-      $parent
-        .css('width', '600px')
-        .css('height', '400px')
+    $child
+      .css('width', '600px')
+      .css('height', '400px')
+    $parent
+      .css('width', '600px')
+      .css('height', '400px')
 
-      const myChart1 = echarts.init($child[0])
-      const option1 = {
-        title: {
-          text: '儿童自我状态',
-          left: 'center'
-        },
-        tooltip: {
-          trigger: 'item'
-        },
-        legend: {
-          orient: 'vertical',
-          left: 'left',
-        },
-        series: [
-          {
-            // name: '访问来源',
-            type: 'pie',
-            radius: '50%',
-            data: [
-              {value: 50, name: '自然型儿童'},
-              {value: 50, name: '适应型儿童'},
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
+    const myChart1 = echarts.init($child[0])
+    const option1 = {
+      title: {
+        text: '儿童自我状态',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+      },
+      series: [
+        {
+          // name: '访问来源',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            {value: 50, name: '自然型儿童'},
+            {value: 50, name: '适应型儿童'},
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
             }
           }
-        ]
-      }
-      option1 && myChart1.setOption(option1)
+        }
+      ]
+    }
+    option1 && myChart1.setOption(option1)
 
-      const myChart2 = echarts.init($parent[0])
-      const option2 = {
-        title: {
-          text: '父母自我状态',
-          left: 'center'
-        },
-        tooltip: {
-          trigger: 'item'
-        },
-        legend: {
-          orient: 'vertical',
-          left: 'left',
-        },
-        series: [
-          {
-            type: 'pie',
-            radius: '50%',
-            data: [
-              {value: 50, name: '抚育型父母'},
-              {value: 50, name: '挑剔型父母'},
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
+    const myChart2 = echarts.init($parent[0])
+    const option2 = {
+      title: {
+        text: '父母自我状态',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+      },
+      series: [
+        {
+          type: 'pie',
+          radius: '50%',
+          data: [
+            {value: 50, name: '抚育型父母'},
+            {value: 50, name: '挑剔型父母'},
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
             }
           }
-        ]
-      }
-      option2 && myChart2.setOption(option2)
-    })
+        }
+      ]
+    }
+    option2 && myChart2.setOption(option2)
   })
 })
--- require/js/script/main.js
+++ require/js/script/main.js
@@ -1,14 +1,12 @@
 require(['common'], function () {
-  require(['jquery'],function($) {
-    require(['api'], function ($http) {
-      $http.menuList({
-        success: function (res) {
-          require(['layout'], function ({layout}) {
-            layout($('#app'), res.data)
-          })
-        },
-        error: function (err) {},
-      })
+  require(['jquery', 'api'],function($, $http) {
+    $http.menuList({
+      success: function (res) {
+        require(['layout'], function ({layout}) {
+          layout($('#app'), res.data)
+        })
+      },
+      error: function (err) {},
     })
   })
 })
